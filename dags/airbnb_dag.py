76cccfba1972b5af83865460c2a359c9381db138
Tarcísio Marinho de Oliveira <tarcisio.marinho@xpi.com.br>
2020-10-27 21:44:02 -0300

    if name == main

--- dags/airbnb_dag.py
+++ dags/airbnb_dag.py
@@ -21,35 +21,35 @@ default_args = {
     'retry_delay': timedelta(minutes=dag_config['retry_delay_minutes']),
 }
 
-
-with models.DAG("elt_airbnb_dag",
-                schedule_interval=timedelta(days=1),
-                default_args=default_args,
-                description='A simple ELT pipeline example DAG') as dag:
-
-
-    github_to_storage_raw = PythonOperator(
-        task_id='extract_github_to_storage',
-        provide_context=False,
-        catchup=False,
-        python_callable=StorageHook.upload_blobs,
-        op_kwargs={"bucket_name": "toureyes-data-lake",
-                   "file_names_list": FILE_NAMES,
-                   "github_url": GITHUB_BASE_URL_DOWNLOAD,
-                   "storage_folder_path": "datasprints/raw"},
-        dag=dag,
-    )
+if __name__ == "__main__":
+    with models.DAG("elt_airbnb_dag",
+                    schedule_interval=timedelta(days=1),
+                    default_args=default_args,
+                    description='A simple ELT pipeline example DAG') as dag:
 
 
-    storage_raw_to_refined = PythonOperator(
-        task_id='storage_raw_to_refined',
-        provide_context=False,
-        catchup=False,
-        python_callable=StorageHook.transform_blobs,
-        op_kwargs={"bucket_name": "toureyes-data-lake",
-                   "blobs_path": "datasprints/raw",
-                   "destination_blobs_path": "datasprints/refined"},
-        dag=dag,
-    )
+        github_to_storage_raw = PythonOperator(
+            task_id='extract_github_to_storage',
+            provide_context=False,
+            catchup=False,
+            python_callable=StorageHook.upload_blobs,
+            op_kwargs={"bucket_name": "toureyes-data-lake",
+                    "file_names_list": FILE_NAMES,
+                    "github_url": GITHUB_BASE_URL_DOWNLOAD,
+                    "storage_folder_path": "datasprints/raw"},
+            dag=dag,
+        )
 
-    github_to_storage_raw >> storage_raw_to_refined
+
+        storage_raw_to_refined = PythonOperator(
+            task_id='storage_raw_to_refined',
+            provide_context=False,
+            catchup=False,
+            python_callable=StorageHook.transform_blobs,
+            op_kwargs={"bucket_name": "toureyes-data-lake",
+                    "blobs_path": "datasprints/raw",
+                    "destination_blobs_path": "datasprints/refined"},
+            dag=dag,
+        )
+
+        github_to_storage_raw >> storage_raw_to_refined
